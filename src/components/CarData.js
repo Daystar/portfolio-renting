6bfb7f53e4278f2401ad06b41894de068de7ef5d
Daystar <er.sagarkoli@gmail.com>
2023-03-13 19:54:16 +0530

    Completed Rental Website

---
+++ src/components/CarData.js
@@ -0,0 +1,93 @@
+import Golf6 from "../images/cars-big/golf6.jpg";
+import AudiA1 from "../images/cars-big/audia1.jpg";
+import Toyota from "../images/cars-big/toyotacamry.jpg";
+import Bmw320 from "../images/cars-big/bmw320.jpg";
+import Benz from "../images/cars-big/benz.jpg";
+import Passat from "../images/cars-big/passatcc.jpg";
+
+export const CAR_DATA = [
+  [
+    {
+      name: "VW Golf 6",
+      price: "3700",
+      img: Golf6,
+      model: "Golf 6",
+      mark: "Volkswagen",
+      year: "2008",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "Audi A1 S-Line",
+      price: "1500",
+      img: AudiA1,
+      model: "Audi",
+      mark: "A1",
+      year: "2012",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Gasoline",
+    },
+  ],
+  [
+    {
+      name: "Toyota Camry",
+      price: "3000",
+      img: Toyota,
+      model: "Camry",
+      mark: "Toyota",
+      year: "2006",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Hybrid",
+    },
+  ],
+  [
+    {
+      name: "BMW 320 ModernLine",
+      price: "3500",
+      img: Bmw320,
+      model: "320",
+      mark: "BMW",
+      year: "2012",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "Mercedes-Benz GLK",
+      price: "5000",
+      img: Benz,
+      model: "Benz GLK",
+      mark: "Mercedes",
+      year: "2006",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Manual",
+      fuel: "Diesel",
+    },
+  ],
+  [
+    {
+      name: "VW Passat CC",
+      price: "2500",
+      img: Passat,
+      model: "Passat CC",
+      mark: "Volkswagen",
+      year: "2008",
+      doors: "4/5",
+      air: "Yes",
+      transmission: "Automatic",
+      fuel: "Gasoline",
+    },
+  ],
+];
