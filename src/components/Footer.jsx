1710699567c06b7d7ac1a78b46ac56998a379fd8
Daystar <er.sagarkoli@gmail.com>
2023-03-14 13:33:28 +0530

    Changed the logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
           <div className="footer-content">
             <ul className="footer-content__1">
               <li>
-                <span>CAR</span> Rental
+                <span>CAR</span> Lelo
               </li>
               <li>
                 We offers a big range of vehicles for all your driving needs. We
